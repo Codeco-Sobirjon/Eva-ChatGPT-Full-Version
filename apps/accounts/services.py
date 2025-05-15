1128a7267c9220d41cdbfd4890d3051980edef31
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2025-05-15 11:03:52 +0500

    finished

--- apps/accounts/services.py
+++ apps/accounts/services.py
@@ -27,7 +27,7 @@ class GoogleLoginService:
             "code": code,
             "client_id": settings.GOOGLE_CLIENT_ID,
             "client_secret": settings.GOOGLE_CLIENT_SECRET,
-            "redirect_uri": 'https://eva-three-mu.vercel.app/api/auth/google/callback/',
+            "redirect_uri": 'https://askeva.ru/api/auth/google/callback/',
             "grant_type": "authorization_code",
         }
         response = requests.post(self.TOKEN_URL, data=data)
